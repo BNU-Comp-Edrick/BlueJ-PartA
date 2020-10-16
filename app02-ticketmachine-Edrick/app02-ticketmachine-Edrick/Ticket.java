6b447c900cb7f6a071324f6708c9de2022d1e772
EY22014511 <71990124+EY22014511@users.noreply.github.com>
2020-10-16 12:27:43 +0100

    modified my ticket class
    I redone the code to change how the information on the ticket would look.
    I also added a getPrice and getDestination in order for the ticket to get the price and the destination selected by the user.

--- app02-ticketmachine-Edrick/app02-ticketmachine-Edrick/Ticket.java
+++ app02-ticketmachine-Edrick/app02-ticketmachine-Edrick/Ticket.java
@@ -1,16 +1,20 @@
 import java.util.Date;
 
 /**
- * Write a description of class Ticket here.
+ * This class will hold infromation about a ticket
+ * this will include the price, issued date and time
+ * and the destination
  *
  * @author Edrick Yapi(22014511)
  * @version 09/10/2020
+ * @modified 16/10/2020
  */
 public class Ticket
 {
     // Attribute, Fields, Variables
     private String destination;
     
+    //currency is in pence
     private int price;
     
     private Date issueDateTime;
@@ -28,14 +32,28 @@ public class Ticket
       issueDateTime = new Date();
     }
     
+    public int getPrice()
+    {
+        return price;
+    }
+    
+    public String getDestination()
+    {
+        return destination;
+    }
+    
     /**
      * 
      */
     public void print()
     {
-        System.out.println("Ticket " + destination +
-           " Price: " + price +
-           " Issued: " + issueDateTime);
+        System.out.println();
+        System.out.print("Destination: " + destination);
+        System.out.println();
+        System.out.print("Price: " + price + "p");
+        System.out.println();
+        System.out.print(issueDateTime);
+        System.out.println();
     }
 
 }
